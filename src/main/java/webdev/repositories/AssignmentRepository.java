64014e10b8f5da2950b663d7bc361e3a4fc4a34d
Aparna Sharma <sharma.apa@husky.neu.edu>
2018-06-04 23:58:06 -0400

    Assignment Service created

--- src/main/java/webdev/repositories/AssignmentRepository.java
+++ src/main/java/webdev/repositories/AssignmentRepository.java
@@ -1,7 +1,15 @@
 package webdev.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import webdev.models.Assignment;
+import webdev.models.Topic;
+
+import java.util.List;
 
 public interface AssignmentRepository extends JpaRepository<Assignment,Integer> {
+
+    @Query("select w from  Widget w where w.topic=:topic and w.widgetType=:widgetType")
+    public List<Assignment> findAssignmentsForTopic(@Param("topic") Topic topic, @Param("widgetType") String widgetType);
 }
---
+++ src/main/java/webdev/services/AssignmentService.java
@@ -0,0 +1,77 @@
+package webdev.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import webdev.models.Assignment;
+import webdev.models.Topic;
+import webdev.repositories.AssignmentRepository;
+import webdev.repositories.TopicRepository;
+
+import java.util.List;
+
+@RestController
+public class AssignmentService {
+
+    private AssignmentRepository assignmentRepository;
+    private TopicRepository topicRepository;
+
+    @Autowired
+    public AssignmentService(AssignmentRepository assignmentRepository, TopicRepository topicRepository) {
+
+        this.assignmentRepository = assignmentRepository;
+        this.topicRepository = topicRepository;
+    }
+
+    @GetMapping("/api/assignment")
+    public List<Assignment> findAllAssignments() {
+
+        return assignmentRepository.findAll();
+    }
+
+    @GetMapping("/api/assignment/{aid}")
+    public Assignment findAssignmentById(@PathVariable("aid") int aid) {
+
+        return assignmentRepository.findById(aid).orElse(null);
+    }
+
+    @GetMapping("/api/topic/{tid}/assignment")
+    public List<Assignment> findAssignmentForTopic(@PathVariable("tid") int tid) {
+
+        Topic topic = topicRepository.findById(tid).orElse(null);
+
+        if (topic != null) {
+
+            return assignmentRepository.findAssignmentsForTopic(topic, "Assignment");
+        }
+        return null;
+    }
+
+
+    @PostMapping("/api/lesson/{tid}/assignment")
+    public Assignment createAssignment(@RequestBody Assignment newAssignment, @PathVariable("tid") int tid) {
+
+        int assignmentId = newAssignment.getId();
+        Assignment existing = assignmentRepository.findById(assignmentId).orElse(null);
+        Topic topic = topicRepository.findById(tid).orElse(null);
+
+        if ((existing == null) && (topic != null)) {
+
+            newAssignment.setTopic(topic);
+            return assignmentRepository.save(newAssignment);
+        }
+
+        return existing;
+    }
+
+
+    @DeleteMapping("/api/assignment/{aid}")
+    public void deleteAssignment(@PathVariable("aid") int aid) {
+
+        Assignment existing = assignmentRepository.findById(aid).orElse(null);
+
+        if (existing != null) {
+            assignmentRepository.delete(existing);
+        }
+    }
+
+}
